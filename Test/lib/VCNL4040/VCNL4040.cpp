00032cd983bdb579366c472956aff0b181be1a11
Malte Nilges <malte.nilges@hotmail.de>
2023-11-13 15:15:11 +0100

    VCNL4040 removed Adafruit dependencies, API cahnges

---
+++ Test/lib/VCNL4040/VCNL4040.cpp
@@ -0,0 +1,280 @@
+
+
+/*!
+ *  @file VCNL4040.cpp
+ *
+ *  @mainpage VCNL4040 proximity and ambient light sensor library
+ *
+ *  @section intro_sec Introduction
+ *
+ * 	I2C Driver for the VCNL4040 proximity and ambient light sensor library
+ *
+ *  @section author Author
+ *
+ *  Malte Nilges
+ *  Bryan Siepert for Adafruit Industries
+ *
+ * 	@section license License
+ *
+ * 	BSD (see license.txt)
+ */
+
+#include "Arduino.h"
+#include <Wire.h>
+
+#include "VCNL4040.hpp"
+
+
+/*!
+ *    @brief  Instantiates a new VCNL4040 class
+ *    @param  wire
+ *            The Wire object to be used for I2C connections.
+ *    @param  i2c_address
+ *            The I2C address to be used.
+ */
+VCNL4040::VCNL4040(TwoWire *wire, uint8_t i2c_addr = VCNL4040_I2CADDR_DEFAULT) {
+  wire = wire;
+  address = i2c_addr;
+
+  als_conf.rawData = 0x0001;
+  als_thdHigh.rawData = 0x0000;
+  als_thdLow.rawData = 0x0000;
+  ps_conf12.rawData = 0x0001;
+  ps_conf3Ms.rawData = 0x0000;
+  ps_canc.rawData = 0x0000;
+  ps_thdLow.rawData = 0x0000;
+  ps_thdLow.rawData = 0x0000;
+}
+
+
+/*!
+ *    @brief  Initializes the settings
+ *    @return True if initialization was successful, otherwise false.
+ */
+boolean VCNL4040::init() {
+  return _init();
+}
+
+boolean VCNL4040::_init() {
+  enableProximity(true);
+  enableWhiteLight(true);
+  enableAmbientLight(true);
+  setProximityHighResolution(true);
+
+  return true;
+}
+/**************** Sensor Data Getters *************************************/
+
+/*!
+    @brief Gets the current proximity sensor value.
+    @return The current proximity measurement in units
+*/
+uint16_t VCNL4040::getProximity() {
+  return readRegister(VCNL4040_REG__PS_DATA);
+}
+
+/*!
+    @brief Gets the current ambient light sensor value.
+    @return The current ambient light measurement in units
+*/
+uint16_t VCNL4040::getAmbientLight() {
+  return readRegister(VCNL4040_REG__ALS_DATA);
+}
+
+/*!
+    @brief Gets the current white light value.
+    @return The current white light measurement in units
+*/
+uint16_t VCNL4040::getWhiteLight() {
+  return readRegister(VCNL4040_REG__WHITE_DATA);
+}
+
+
+/*!
+    @brief Gets the current ambient light sensor in Lux.
+    @return The current ambient light measurement in Lux
+*/
+float VCNL4040::getLux() {
+  // scale the lux depending on the value of the integration time
+  // see page 8 of the VCNL4040 application note:
+  // https://www.vishay.com/docs/84307/designingvcnl4040.pdf
+  return (readRegister(VCNL4040_REG__ALS_DATA) * (0.1 / (1 << (uint8_t)getAmbientIntegrationTime())));
+}
+/**************** Sensor Enable Functions   *******************************/
+
+
+/*!
+    @brief Enables or disables proximity measurements.
+    @param  enable
+            Set to true to enable proximity measurements,
+            set to false to disable.
+*/
+void VCNL4040::enableProximity(bool enable) {
+  ps_conf12.sd = (uint8_t) enable;
+  writeRegister(VCNL4040_REG__PS_CONF1_2, ps_conf12.rawData);
+}
+
+/*!
+    @brief Enables ambient light measurements
+    @param  enable
+            Set to true to enable ambient light measurements,
+            set to false to disable.
+*/
+void VCNL4040::enableAmbientLight(bool enable) {
+  als_conf.sd = (uint8_t) enable;
+  writeRegister(VCNL4040_REG__ALS_CONF1, als_conf.rawData);
+}
+
+/*!
+    @brief Enables white light measurements
+    @param  enable
+            Set to true to enable white light measurements,
+            set to false to disable.
+*/
+void VCNL4040::enableWhiteLight(bool enable) {
+  ps_conf3Ms.white_en = (uint8_t) enable;
+  writeRegister(VCNL4040_REG__PS_CONF3_MS, ps_conf3Ms.rawData);
+}
+
+/******************** Tuning Functions ********************************** */
+
+
+/*!
+    @brief Gets the integration time for proximity sensing measurements.
+    @returns The integration time being used for proximity measurements.
+*/
+VCNL4040_ProximityIntegration VCNL4040::getProximityIntegrationTime() {
+  return (VCNL4040_ProximityIntegration) ps_conf12.it;
+}
+
+/*!
+    @brief Sets the integration time for proximity sensing measurements.
+    @param  integration_time
+            The integration time to use for proximity measurements. Must be a
+            `VCNL4040_ProximityIntegration`.
+*/
+void VCNL4040::setProximityIntegrationTime(VCNL4040_ProximityIntegration integration_time) {
+  ps_conf12.it = (uint8_t) integration_time;
+  writeRegister(VCNL4040_REG__PS_CONF1_2, ps_conf12.rawData);
+}
+
+
+/*!
+    @brief Gets the integration time for ambient light sensing measurements.
+    @returns The integration time being used for ambient light measurements.
+*/
+VCNL4040_AmbientIntegration VCNL4040::getAmbientIntegrationTime() {
+  return (VCNL4040_AmbientIntegration) als_conf.it;
+}
+
+
+/*!
+    @brief Sets the integration time for ambient light sensing measurements.
+    @param  integration_time
+            The integration time to use for ambient light measurements. Must be
+   a `VCNL4040_AmbientIntegration`.
+*/
+void VCNL4040::setAmbientIntegrationTime(VCNL4040_AmbientIntegration integration_time) {
+  // delay according to the integration time to let the reading at the old IT
+  // clear out
+  uint16_t old_it_ms = ((8 << als_conf.it) * 10);
+  uint16_t new_it_ms = ((8 << (uint8_t)integration_time) * 10);
+
+  als_conf.it = (uint8_t)integration_time;
+  writeRegister(VCNL4040_REG__ALS_CONF1, als_conf.rawData);
+  delay((old_it_ms + new_it_ms + 1));
+}
+
+
+/*!
+    @brief Gets the current for the LED used for proximity measurements.
+    @returns The LED current value being used for proximity measurements.
+*/
+VCNL4040_LEDCurrent VCNL4040::getProximityLEDCurrent() {
+  return (VCNL4040_LEDCurrent)ps_conf3Ms.led_i;
+}
+
+/*!
+    @brief Sets the current for the LED used for proximity measurements.
+    @param  led_current
+            The current value to be used for proximity measurements. Must be a
+            `VCNL4040_LEDCurrent`.
+*/
+void VCNL4040::setProximityLEDCurrent(VCNL4040_LEDCurrent led_current) {
+  ps_conf3Ms.led_i = (uint8_t) led_current;
+  writeRegister(VCNL4040_REG__PS_CONF3_MS, ps_conf3Ms.rawData);
+}
+
+
+/*!
+    @brief Sets the duty cycle for the LED used for proximity measurements.
+    @returns The duty cycle value being used for proximity measurements.
+*/
+VCNL4040_LEDDutyCycle VCNL4040::getProximityLEDDutyCycle() {
+  return (VCNL4040_LEDDutyCycle)ps_conf12.duty;
+}
+
+/*!
+    @brief Sets the duty cycle for the LED used for proximity measurements.
+    @param  duty_cycle
+            The duty cycle value to be used for proximity measurements. Must be
+   a `VCNL4040_LEDDutyCycle`.
+*/
+void VCNL4040::setProximityLEDDutyCycle(VCNL4040_LEDDutyCycle duty_cycle) {
+  ps_conf12.duty = (uint8_t) duty_cycle;
+  writeRegister(VCNL4040_REG__PS_CONF1_2, ps_conf12.rawData);
+}
+
+
+/*!
+    @brief Gets the resolution of proximity measurements
+    @return The current proximity measurement resolution
+            If true, proximity measurements are 16-bit,
+            If false, proximity measurements are 12-bit,
+*/
+bool VCNL4040::getProximityHighResolution() {
+  return (bool)ps_conf12.hd;
+}
+
+/*!
+    @brief Sets the resolution of proximity measurements
+    @param  high_resolution
+            Set to true to take 16-bit measurements for proximity,
+            set to faluse to use 12-bit measurements.
+*/
+void VCNL4040::setProximityHighResolution(bool high_resolution) {
+  ps_conf12.hd = (uint8_t) high_resolution;
+  writeRegister(VCNL4040_REG__PS_CONF1_2, ps_conf12.rawData);
+}
+
+/*!
+    @brief Writes a register with specified data
+    @param reg Register to write to
+    @param data Data to write
+*/
+void VCNL4040::writeRegister(uint8_t reg, uint16_t data) {
+  wire->beginTransmission(address);
+	wire->write(reg);
+  wire->write(lowByte(data));
+  wire->write(highByte(data));
+	wire->endTransmission();
+}
+
+/*!
+    @brief  Reads the data stored within the specified register
+    @param  reg Register to be read.
+    @return Data stored in register
+ */
+uint16_t VCNL4040::readRegister(uint8_t reg) {
+  wire->beginTransmission(address);
+	wire->write(reg);
+	wire->endTransmission();
+
+	wire->requestFrom(address, (uint8_t)2);
+
+	uint8_t lsb = wire->read();
+	uint8_t msb = wire->read();
+
+	uint16_t val = msb << 8 | lsb;
+  return val;
+}
---
+++ Test/lib/VCNL4040/VCNL4040.hpp
@@ -0,0 +1,213 @@
+/*!
+ *  @file VCNL4040.hpp
+ *
+ * 	I2C Driver for VCNL4040 proximity and ambient light sensor
+ *
+ *	BSD license (see license.txt)
+ */
+
+#ifndef _VCNL4040_HPP
+#define _VCNL4040_HPP
+
+#include "Arduino.h"
+#include <Wire.h>
+#define VCNL4040_I2CADDR_DEFAULT 0x60 ///< VCNL4040 default i2c address
+
+// All addresses are for 16bit registers;
+// duplicates are for high or low bytes that aren't used together
+#define VCNL4040_REG__ALS_CONF1 0x00     ///< Ambient light sensor configuration register
+#define VCNL4040_REG__ALS_THDH 0x01      ///< Ambient light high threshold register
+#define VCNL4040_REG__ALS_THDL 0x02      ///< Ambient light low threshold register
+#define VCNL4040_REG__PS_CONF1_2 0x03    ///< Proximity sensor configuration 1/2 register
+#define VCNL4040_REG__PS_CONF3_MS 0x04   ///< Proximity sensor configuration 3/MS register
+#define VCNL4040_REG__PS_THDL 0x06       ///< Proximity sensor low threshold register
+#define VCNL4040_REG__PS_THDH 0x07       ///< Proximity sensor high threshold register
+#define VCNL4040_REG__PS_DATA 0x08       ///< Proximity sensor data register
+#define VCNL4040_REG__ALS_DATA 0x09      ///< Ambient light sensor data register
+#define VCNL4040_REG__WHITE_DATA 0x0A    ///< White light sensor data register
+#define VCNL4040_REG__INT_FLAG 0x0B      ///< Interrupt status register
+#define VCNL4040_REG__DEVICE_ID 0x0C     ///< Device ID
+
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t sd : 1;
+    uint8_t int_en : 1;
+    uint8_t pers : 2;
+    uint8_t reserved1 : 2;
+    uint8_t it : 2;
+    uint8_t reserved2 : 8;
+	};
+} VCNL4040_ALSConfReg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t thdh_lo : 8;
+    uint8_t thdh_hi : 8;
+	};
+} VCNL4040_ALSThdHighReg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t thdl_lo : 8;
+    uint8_t thdl_hi : 8;
+	};
+} VCNL4040_ALSThdLowReg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+    uint8_t sd : 1;
+    uint8_t it : 3;
+    uint8_t pers : 2;
+    uint8_t duty : 2;
+    uint8_t intr : 2;
+    uint8_t reserved1 : 1;
+    uint8_t hd :  1;
+    uint8_t reserved2 : 4;
+	};
+} VCNL4040_PSConf12Reg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t sc_en : 1;
+    uint8_t reserved1 : 1;
+    uint8_t trig : 1;
+    uint8_t af : 1;
+    uint8_t smart_pers : 1;
+    uint8_t mps : 2;
+    uint8_t reserved2 : 1;
+    uint8_t led_i : 3;
+    uint8_t reserved3 : 3;
+    uint8_t ms : 1;
+    uint8_t white_en : 1;
+	};
+} VCNL4040_PSConf3MSReg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t canc_lo : 8;
+    uint8_t canc_hi : 8;
+	};
+} VCNL4040_PSCancReg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t thdh_lo : 8;
+    uint8_t thdh_hi : 8;
+	};
+} VCNL4040_PSThdHighReg;
+
+typedef union {
+	uint16_t rawData;
+	struct {
+		uint8_t thdl_lo : 8;
+    uint8_t thdl_hi : 8;
+	};
+} VCNL4040_PSThdLowReg;
+
+
+enum class VCNL4040_LEDDutyCycle { LED_DUTY_1_40, LED_DUTY_1_80, LED_DUTY_1_160, LED_DUTY_1_320 };
+enum class VCNL4040_LEDCurrent {
+  LED_CURRENT_50MA,
+  LED_CURRENT_75MA,
+  LED_CURRENT_100MA,
+  LED_CURRENT_120MA,
+  LED_CURRENT_140MA,
+  LED_CURRENT_160MA,
+  LED_CURRENT_180MA,
+  LED_CURRENT_200MA,
+};
+
+enum class VCNL4040_AmbientIntegration {
+  AMBIENT_INTEGRATION_TIME_80MS = 0,
+  AMBIENT_INTEGRATION_TIME_160MS,
+  AMBIENT_INTEGRATION_TIME_320MS,
+  AMBIENT_INTEGRATION_TIME_640MS,
+};
+enum class VCNL4040_ProximityIntegration {
+  PROXIMITY_INTEGRATION_TIME_1T = 0,
+  PROXIMITY_INTEGRATION_TIME_1T5,
+  PROXIMITY_INTEGRATION_TIME_2T,
+  PROXIMITY_INTEGRATION_TIME_2T5,
+  PROXIMITY_INTEGRATION_TIME_3T,
+  PROXIMITY_INTEGRATION_TIME_3T5,
+  PROXIMITY_INTEGRATION_TIME_4T,
+  PROXIMITY_INTEGRATION_TIME_8T,
+};
+enum class VCNL4040_ProximityType { PROXIMITY_INT_DISABLE, PROXIMITY_INT_CLOSE, PROXIMITY_INT_AWAY, PROXIMITY_INT_CLOSE_AWAY };
+enum class VCNL4040_InterruptType { PROXIMITY_AWAY, PROXIMITY_CLOSE, AMBIENT_HIGH = 4, AMBIENT_LOW };
+
+/*!
+ *    @brief  Class that stores state and functions for interacting with
+ *            the VCNL4040 I2C Digital Potentiometer
+ */
+class VCNL4040 {
+public:
+  VCNL4040(TwoWire *wire, uint8_t i2c_addr = VCNL4040_I2CADDR_DEFAULT);
+  boolean init();
+  uint16_t getProximity();
+  uint16_t getAmbientLight();
+  uint16_t getWhiteLight();
+  float getLux();
+
+  void enableProximity(bool enable);
+  void enableAmbientLight(bool enable);
+  void enableWhiteLight(bool enable);
+
+  // Interrupts
+  /*
+  uint8_t getInterruptStatus();
+  void enableAmbientLightInterrupts(bool enable);
+  uint16_t getAmbientLightHighThreshold();
+  void setAmbientLightHighThreshold(uint16_t high_threshold);
+  uint16_t getAmbientLightLowThreshold();
+  void setAmbientLightLowThreshold(uint16_t low_threshold);
+
+  void enableProximityInterrupts(VCNL4040_ProximityType interrupt_condition);
+  uint16_t getProximityLowThreshold();
+  void setProximityLowThreshold(uint16_t low_threshold);
+  uint16_t getProximityHighThreshold();
+  void setProximityHighThreshold(uint16_t high_threshold);
+  */
+
+  VCNL4040_ProximityIntegration getProximityIntegrationTime();
+  void setProximityIntegrationTime(VCNL4040_ProximityIntegration integration_time);
+
+  VCNL4040_AmbientIntegration getAmbientIntegrationTime();
+  void setAmbientIntegrationTime(VCNL4040_AmbientIntegration integration_time);
+
+  VCNL4040_LEDCurrent getProximityLEDCurrent();
+  void setProximityLEDCurrent(VCNL4040_LEDCurrent led_current);
+
+  VCNL4040_LEDDutyCycle getProximityLEDDutyCycle();
+  void setProximityLEDDutyCycle(VCNL4040_LEDDutyCycle duty_cycle);
+
+  bool getProximityHighResolution();
+  void setProximityHighResolution(bool high_resolution);
+
+private:
+  bool _init();
+  TwoWire* wire;
+  uint8_t address;
+
+  VCNL4040_ALSConfReg als_conf;
+  VCNL4040_ALSThdLowReg als_thdLow;
+  VCNL4040_ALSThdHighReg als_thdHigh;
+  VCNL4040_PSConf12Reg ps_conf12;
+  VCNL4040_PSConf3MSReg ps_conf3Ms;
+  VCNL4040_PSCancReg ps_canc;
+  VCNL4040_PSThdLowReg ps_thdLow;
+  VCNL4040_PSThdHighReg ps_thdHigh;
+
+  void writeRegister(uint8_t reg, uint16_t data);
+  uint16_t readRegister(uint8_t reg);
+};
+
+#endif
