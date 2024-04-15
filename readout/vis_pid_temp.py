7529ddac3bbe7675bfcbff718abbf2d360af644b
Malte Nilges <malte.nilges@hotmail.de>
2024-04-15 11:18:48 +0200

    Updated PID visualization

--- readout/vis_pid_temp.py
+++ readout/vis_pid_temp.py
@@ -1,29 +1,50 @@
+
+import matplotlib as mpl
+# Use the pgf backend (must be set before pyplot imported)
+PLOTOPTION = 'pdf'
+PLOTSIZE = 0.9
+if PLOTOPTION == 'pgf':
+    mpl.use('pgf')
+
 import csv
 import matplotlib.pyplot as plt
-import datetime
+import StyleHelper as SH
+SH.update_pyplot_rcParams()
+
 
 x = []
 y1 = [] 
 y2 = []
 
-with open('test3_pid.csv') as csvfile:
+with open('pid_from_t10rh33.4.csv') as csvfile:
     reader = csv.reader(csvfile)
     # next(reader) # skip header row
     t = 0.0
     for row in reader:
         x.append(t) 
-        y1.append(float(row[9]))  
-        # y2.append(float(row[1]))
+        y1.append(float(row[1]))  
+        y2.append(float(row[0]))
         t = t + 0.2
         
-fig, ax = plt.subplots()
-ax.plot(x, y1, label='Actual')
-# ax.plot(x, y2, label='Target')
+fig, ax = plt.subplots(figsize=SH.set_size(SH.TEXTWIDTH_MASTER * PLOTSIZE), layout='constrained')
+ax.plot(x, y2, label='Target', c=SH.IES_BLUE_100)
+ax.plot(x, y1, label='Measured', c=SH.IES_RED_100)
 
-ax.set(xlabel='Time', ylabel='Temperature (C)',
-       title='Temperature over Time')
+ax.set(xlabel='Time [\\unit{\s}]', ylabel='Temperature [\\unit{\celsius}]')
 ax.grid()
 
 fig.autofmt_xdate()
+
+
+ax.margins(0.0, 0.04)
+ax.relim()
+# update ax.viewLim using the new dataLim
+ax.autoscale_view()
+
 plt.legend()
-plt.show()  
+
+if PLOTOPTION == 'pgf':
+    plt.savefig('lolza.pgf', format = 'pgf')
+else:
+    plt.savefig('pid_offset.pdf', bbox_inches='tight')
+    plt.show()
